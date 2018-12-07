01c9f99fb52945f8de503c0c80f95dc2d6fb53ff
Vidhya2796 <44641606+Vidhya2796@users.noreply.github.com>
2018-12-07 06:43:56 +0530

    Add files via upload

---
+++ LoginServlet.java
@@ -0,0 +1,37 @@
+package com.vidhyac.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String uname=request.getParameter("uname");
+		String pass=request.getParameter("pass");
+		
+		if (uname.equals("vidhya") && pass.equals("cvs")) {
+			HttpSession session= request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+		}else{
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+}
